296b5ad41a7d49e6be52745d1688de9a6a6890f1
unknown <hindhujhasridhar@gmail.com>
2022-01-10 23:34:20 +0530

    QUEUE IS CREATED

--- Stack_Queue/Program.cs
+++ Stack_Queue/Program.cs
@@ -10,16 +10,24 @@ namespace Stack_Queue
         static void Main(string[] args)
         {
             Console.WriteLine("STACK AND QUEUE PROBLEMS");
+            //STACK
             Stack<int> stack =new Stack<int>();
             //pushing values into stack
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
             //peeking values in stack
-            Console.WriteLine("PEEKING STACK:" + stack.Peek());
+            Console.WriteLine(stack.Peek());
             stack.Pop();
             //After Removing elements from stack
             Console.WriteLine(stack.Peek());
+
+            //QUEUE
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            Console.WriteLine(queue.Peek());
 
         }
     }
