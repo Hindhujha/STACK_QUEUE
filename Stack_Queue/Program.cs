42d4e7b90876c48a729b7e8c3994e8327dd8943c
unknown <hindhujhasridhar@gmail.com>
2022-01-10 22:26:02 +0530

    STACK QUEUE PROBLEMS

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("STACK AND QUEUE PROBLEMS");
+        }
+    }
+}
